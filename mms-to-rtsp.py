2bf82cfa48bc4385fc1f7bd8a885732d52c3edf1
Hagai Kariti <hkariti@gmail.com>
2016-10-25 19:08:42 +0300

    Fix stupid errors about var names, etc

--- mms-to-rtsp.py
+++ mms-to-rtsp.py
@@ -21,13 +21,14 @@ URL should start with mms:// or rtsp://
 def patch_server_data(buf, state):
     ## Patch OPTIONS response to include GET_PARAMETER as public method
     rtsp_index = buf.find("RTSP/1.0")
+    temp_buf = ''
     if rtsp_index != -1:
-        target.send(buf[:rtsp_index])
+        temp_buf = buf[:rtsp_index]
         buf = buf[rtsp_index:]
         print "Patching OPTIONS response, adding GET_PARAMETER to public methods"
         buf = buf.replace("\r\n","\r\nPublic: GET_PARAMETER\r\n", 1)
         state['options_patch_needed'] = False
-    return buf
+    return temp_buf + buf
 
 def patch_vlc_data(buf, state):
     ## OPTIONS request are important. We get the escaped url to be used later in other requests,
@@ -40,32 +41,33 @@ def patch_vlc_data(buf, state):
 
     ## PLAY/PAUSE/GET_PARAMETER requests have an escaped URI that confused the server, replace it with
     ## an unescaped URI that we previously got from the OPTIONS request
-    if original_url:
+    if state['original_url']:
         match = re.search('(PLAY|PAUSE|GET_PARAMETER) ([^ ]+) RTSP/1.0', buf)
         if match:
-            buf = re.sub('(PLAY|PAUSE|GET_PARAMETER) [^ ]+ RTSP/1.0', '\\1 %s RTSP/1.0' % original_url, buf)
-            print "Replaced %s uri.\n  Orig: %s\n  Repl: %s" % (match.groups()[0], match.groups()[1], original_url)
+            buf = re.sub('(PLAY|PAUSE|GET_PARAMETER) [^ ]+ RTSP/1.0', '\\1 %s RTSP/1.0' % state['original_url'], buf)
+            print "Replaced %s uri.\n  Orig: %s\n  Repl: %s" % (match.groups()[0], match.groups()[1], state['original_url'])
+    return buf
 
 def handle_client(host, port, vlc):
     print "Connecting to stream source at %s:%d" % (host, port)
     stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     stream.connect((host, port))
     stream.setblocking(0)
-    stream_file = client.makefile()
+    stream_file = stream.makefile()
     print "Connected"
 
     state = dict(original_url=None, options_patch_needed=False)
     readbuf_size = 1024*1024
     # Read from all buffers with data in them until one is at EOF
     while True:
-        rlist, wlist, elist = select.select([client, vlc], [], [])
+        rlist, wlist, elist = select.select([stream, vlc], [], [])
         for source in rlist:
             if source is stream:
                 source_name = "backend"
                 target = vlc
             else:
                 source_name = "vlc"
-                target = client
+                target = stream
             buf = source.recv(readbuf_size)
             # Check EOF
             if not buf:
